2fcf95ab6295aeb91ff4646a37c6c8c07809b916
AsadBhatti <asadmushtaq555777@gmail.com>
2025-07-16 06:44:13 -0700

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,17 @@
+import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
+import { serveDir } from "https://deno.land/std@0.177.0/http/file_server.ts";
+
+serve(req => {
+    return serveDir(req, {
+        fsRoot: ".", // Serves files from the current directory.
+        urlRoot: "",
+        quiet: true,
+    });
+});
+
+
+
+
+
+
+
